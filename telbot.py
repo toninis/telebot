5321fec72377b10bb75056e9371f956106c82042
Antonis Stamatiou <antonis.stamatiou@upstreamsystems.com>
2017-12-04 13:23:49 +0200

    Final fix , update on click

--- telbot.py
+++ telbot.py
@@ -8,6 +8,7 @@ import pprint
 import argparse
 #import errors
 #### https://api.telegram.org/bot345178316:AAFxqQy7qIA7gJwUM4nmfvpjfXK0EcdUq-Q/getUpdates
+
 global updater , db , t_end
 t_end = time.time() + 60
 updater = Updater(token='345178316:AAFxqQy7qIA7gJwUM4nmfvpjfXK0EcdUq-Q')
@@ -57,76 +58,79 @@ def f_info(bot, update):
 def inl(bot, update):
     query = update.callback_query
     message = query.message.chat
-    user = message.first_name
+    user_info = query.from_user
+    g_name = ''
+    if message.type == "group":
+        user = user_info.first_name
+        g_name = message.title
+    elif message.type == "private":
+        user = message.first_name
     group = message.id
-    logger.info(query)
+    logger.info(query.from_user)
     logger.info(message)
-    #logger.info(update.callback_query)
+
     if query.data == "start":
-        #func for turn on light res = k_light.on()
+        ### send start signal
         bot.answerCallbackQuery(callback_query_id=update.callback_query.id, text="Started!")
-        text=' %s requested to start the service .. ' % user
+        ### edit upon click
+        edit_text = "Selected option : %s from %s " % (query.data , user)
+        bot.edit_message_text(text=edit_text,chat_id=query.message.chat_id,message_id=query.message.message_id)
+        if g_name:
+            text=' %s requested a status update from group %s .. ' % (user,g_name)
+        else:
+            text=' %s requested the status of the service .. ' % user
+
         f_send(bot,group,text)
-        #bot.editMessageText(inline_message_id=update.callback_query.inline_message_id, text="Do you want to turn On or Off light? Light is ON")
-        #hardcoded vars variant
-        #bot.editMessageText(message_id=298, chat_id=174554240, text="Do you want to turn On or Off light? Light is ON")
+
     elif query.data == "status":
-        #func for turn on light res = k_light.off()
+        ### send status signal
         bot.answerCallbackQuery(callback_query_id=update.callback_query.id, text="Find the status below : ")
-        text=' %s requested a status update .. ' % user
+        ### edit upon click
+        edit_text = "Selected option : %s from %s " % (query.data , user )
+        bot.edit_message_text(text=edit_text,chat_id=query.message.chat_id,message_id=query.message.message_id)
+
+        if g_name:
+            text=' %s requested a status update from group %s .. ' % (user,g_name)
+        else:
+            text=' %s requested the status of the service .. ' % user
+
         f_send(bot,group,text)
-        #bot.editMessageText(inline_message_id=update.callback_query.inline_message_id, text="Do you want to turn On or Off light? Light is ON")
-        #hardcoded vars variant
-        #bot.editMessageText(message_id=298, chat_id=174554240, text="Do you want to turn On or Off light? Light is OFF")
+
     elif query.data == "stop":
-        #func for turn on light res = k_light.off()
+        ### send stop signal
         bot.answerCallbackQuery(callback_query_id=update.callback_query.id, text="Terminated!")
-        text=' %s requested a termination .. ' % user
+        ### edit upon click
+        edit_text = "Selected option : %s from %s " % (query.data , user )
+        bot.edit_message_text(text=edit_text,chat_id=query.message.chat_id,message_id=query.message.message_id)
+
+        if g_name:
+            text=' %s requested a status update from group %s .. ' % (user,g_name)
+        else:
+            text=' %s requested the status of the service .. ' % user
+
         f_send(bot,group,text)
-        #bot.editMessageText(inline_message_id=update.callback_query.inline_message_id, text="Do you want to turn On or Off light? Light is ON")
-        #hardcoded vars variant
-        #bot.editMessageText(message_id=298, chat_id=174554240, text="Do you want to turn On or Off light? Light is OFF")
     else:
         logger.error("Error ... ")
 
 def info_menu(bot, update):
-    reply_markup = telegram.InlineKeyboardMarkup([[telegram.InlineKeyboardButton("start", callback_data="start"), telegram.InlineKeyboardButton("stop", callback_data="stop") , telegram.InlineKeyboardButton("status", callback_data="status")]])
-    #update.message.reply_text(
-    ddd = bot.sendMessage(chat_id=update.message.chat_id, text="Choose one of the following actions :", reply_markup=reply_markup)
-    group = update.message.chat_id
-    user = update.message.from_user.first_name
-    mt = f_send(bot,group,"Choose in the next 10 seconds... %s " % user)
-    #time.sleep(10)
-
-    logger.info(update)
-    #inl(bot,update)
-
-def info_test(bot, update):
-    #reply_keyboard = [['Boy', 'Girl', 'Other']]
     reply_markup = telegram.InlineKeyboardMarkup([[telegram.InlineKeyboardButton("start", callback_data="start"), telegram.InlineKeyboardButton("stop", callback_data="stop") , telegram.InlineKeyboardButton("status", callback_data="status")]])
     update.message.reply_text('Choose one of the following actions :',reply_markup=reply_markup)
     return True
 
 def button(bot, update):
     query = update.callback_query
-    bot.edit_message_text(text="Selected option: {}".format(query.data),
-                          chat_id=query.message.chat_id,
-                          message_id=query.message.message_id)
+    bot.edit_message_text(text="Selected option: {}".format(query.data),chat_id=query.message.chat_id,message_id=query.message.message_id)
 
 def callback_handler(bot,update):
     reply_markup = telegram.InlineKeyboardMarkup([[telegram.InlineKeyboardButton("start", callback_data="start"), telegram.InlineKeyboardButton("stop", callback_data="stop") , telegram.InlineKeyboardButton("status", callback_data="status")]])
     update.message.reply_text('Choose one of the following actions :',reply_markup=reply_markup)
-    #dp.add_handler(CallbackQueryHandler(update.message.reply_text('Choose one of the following actions :',reply_markup=reply_markup),inl))
-    #return inl(bot,update)
-
 
 def polling():
     """Polling function ... """
     #### Handlers ####
 
     dp.add_handler(RegexHandler('fuck',f_reply))
-    #dp.add_handler(CommandHandler('info',info_menu))
-    dp.add_handler(CommandHandler('info',info_test))
+    dp.add_handler(CommandHandler('info',info_menu))
     dp.add_handler(CallbackQueryHandler(inl))
 
     #dp.add_error_handler(error)
